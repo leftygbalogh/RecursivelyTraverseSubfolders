bee7165162ef9938d371c9937d92cf4984ff53b9
lefty <lefty@lefty-Inspiron-5558>
2016-03-18 06:13:54 +0100

    added horizontal and vertical traversal functions

--- src/recursivehorizontallibrarytraversal/RecursiveHorizontalLibraryTraversal.java
+++ src/recursivehorizontallibrarytraversal/RecursiveHorizontalLibraryTraversal.java
@@ -3,7 +3,6 @@
  * 
  * Lefty G Balogh
  */
-
 package recursivehorizontallibrarytraversal;
 
 import java.io.BufferedReader;
@@ -12,6 +11,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,47 +22,112 @@ import java.util.logging.Logger;
  */
 public class RecursiveHorizontalLibraryTraversal {
 
-    
     /**
-     * Recursively traverses all subdirectories in the given directory and prints its contents into a .CSV file.
-     * It appends the level of depth where are file/directory set was found
+     * Recursively traverses all subdirectories in the given directory and
+     * prints its contents into a .CSV file. It appends the level of depth where
+     * are file/directory set was found
+     *
      * @param path the directory to go through
      * @param level should be zero
-     * @param targetFilePath the file name and path where the results will be saved
-     *  
+     * @param targetFilePath the file name and path where the results will be
+     * saved
+     *
      */
-    public static void readContents(String path, int level, String targetFilePath){
+    public static void readShallowContentFirst(String path, int level, String targetFilePath) {
         level++;
         File base = new File(path);
-        File[] filesAndDirectories = base.listFiles();        
+        File[] filesAndDirectories = base.listFiles();
         for (File file : filesAndDirectories) {
             System.out.println(file.getName());
-        }        
-        try {        
-            FileWriter writer = new FileWriter(targetFilePath, true);            
+        }
+        try {
+            FileWriter writer = new FileWriter(targetFilePath, true);
             for (File file : filesAndDirectories) {
-                writer.append(file.getName()+',');
+                writer.append(file.getName() + ',');
             }
-            writer.append(Integer.toString(level)+"\n");
+            writer.append(Integer.toString(level) + "\n");
             writer.flush();
-            writer.close();            
+            writer.close();
             for (File file : filesAndDirectories) {
-                if (file.isDirectory()){
-                    readContents(file.getCanonicalPath(), level, targetFilePath);
+                if (file.isDirectory()) {
+                    readShallowContentFirst(file.getCanonicalPath(), level, targetFilePath);
                 }
-            }            
+            }
         } catch (IOException ex) {
             Logger.getLogger(RecursiveHorizontalLibraryTraversal.class.getName()).log(Level.SEVERE, null, ex);
-        }        
+        }
     }
-    
+
+    public static Set<Integer> readDeepContentFirst(String path, Set<Integer> hashTracker, String targetFilePath) {
+
+        File base = new File(path);
+        File[] filesAndDirectories = base.listFiles();
+
+        boolean containsFilesOnly = true;
+        //filesAndDirectories[0].
+
+        for (File file : filesAndDirectories) {
+
+            if (file.isDirectory() && !hashTracker.contains(file.hashCode())) {
+                System.out.println("Unvisited Directory:" + file);
+                hashTracker.add(file.hashCode());
+                appendToFile(targetFilePath, file);
+                hashTracker = readDeepContentFirst(file.getPath(), hashTracker, targetFilePath);
+            }
+
+            if (file.isDirectory()) {
+                containsFilesOnly = false;
+            }
+        }
+
+        if (containsFilesOnly) {
+            for (File file : filesAndDirectories) {
+                hashTracker.add(file.hashCode());
+                appendToFile(targetFilePath, file);
+                System.out.println(file.getName());
+            }
+
+        }
+
+        for (File file : filesAndDirectories) {
+            if (!hashTracker.contains(file.hashCode())) {
+                hashTracker.add(file.hashCode());
+                appendToFile(targetFilePath, file);
+                System.out.println(file.getName());
+            }
+        }
+
+        return hashTracker;
+    }
+
+    public static void appendToFile(String targetFilePath, File visitedFileNode) {
+        try {
+
+            FileWriter writer = new FileWriter(targetFilePath, true);
+
+            writer.append(visitedFileNode.getName() + ", ");
+            if(visitedFileNode.isDirectory()){
+                writer.append("\n");
+            }
+            writer.flush();
+            writer.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(RecursiveHorizontalLibraryTraversal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) {
-        String path = "/home/lefty/path0";
-        String target = "/home/lefty/path0.csv";
-        readContents(path, 0, target);        
-        String path2 = "/home/lefty/Desktop";
-        String target2 = "/home/lefty/desktop.csv";
-        readContents(path2, 0, target2);
+        String path0 = "/home/lefty/path0";
+        String desktop = "/home/lefty/Desktop";
+        String path1 = "/home/lefty/path1";
+
+        String horizontalTarget = "/home/lefty/horizontal.csv";
+        String verticalTarget = "/home/lefty/vertical.csv";
+
+        Set<Integer> hashTracker = new TreeSet<>();
+        readDeepContentFirst(path0, hashTracker, verticalTarget);
+        readShallowContentFirst(path0, 0, horizontalTarget);
     }
-    
+
 }
